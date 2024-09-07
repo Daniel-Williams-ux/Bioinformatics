becb2056fe64fe7faea586dcd89a10bde92f71e4
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 22:59:38 +0100

    Create loop_through_a_list.R

---
+++ list/loop_through_a_list.R
@@ -0,0 +1,13 @@
+# You can loop through the list items by using a for loop:
+
+# Example
+# Print all items in the list, one by one:
+
+thislist <- list("apple", "banana", "cherry")
+
+for (x in thislist) {
+  print(x)
+}
+# [1] "apple"
+# [1] "banana"
+# [1] "cherry"
