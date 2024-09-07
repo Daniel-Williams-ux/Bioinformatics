186143db57608340a26a13f0ec714b43dc9a6586
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 22:52:25 +0100

    Create check_if_item_exsist.R

---
+++ list/check_if_item_exsist.R
@@ -0,0 +1,10 @@
+# Check if Item Exists
+# To find out if a specified item is present in a list, use the %in% operator:
+
+# Example
+# Check if "apple" is present in the list:
+
+thislist <- list("apple", "banana", "cherry")
+
+"apple" %in% thislist
+# TRUE
