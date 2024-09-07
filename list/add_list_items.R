0687b726e069dfb0682828a74c08187d3c5f8ecb
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 23:04:16 +0100

    Create add_list_items.R

---
+++ list/add_list_items.R
@@ -0,0 +1,8 @@
+# To add an item to the end of the list, use the append() function:
+
+# Example
+# Add "orange" to the list:
+
+thislist <- list("apple", "banana", "cherry")
+
+append(thislist, "orange")
