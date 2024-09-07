078de74d5af78858d4a859018a1edf241a2d4287
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 18:10:28 +0100

    Create vectorized_operation.R

---
+++ R/vectors/vectorized_operation.R
@@ -0,0 +1,11 @@
+# A vectorized operation applies an operation to a vector element-wise:
+
+# 1. # Create a vector named "items":
+items <- c(5,10,15,20,25,30,35,40)
+items
+
+# 2. # Divide each item in a vector by 5, and return a new vector containing the result
+items/5 # 1 2 3 4 5 6 7 8
+
+# 3. # Multiply two vectors together (element-wise).  Return a new vector with the results:
+c(1,2,3) * c(2,4,6) # 2, 8 18
