dd19e75c890a66289cd37dad4402e107bc3fe72a
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 23:01:53 +0100

    Create range_of_indexes.R

---
+++ list/range_of_indexes.R
@@ -0,0 +1,12 @@
+# You can specify a range of indexes by specifying where to start and where to end the range, by using the : operator:
+
+# Example
+# Return the second, third, fourth and fifth item:
+
+thislist <- list("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+(thislist)[2:5]
+# 'banana'
+# 'cherry'
+# 'orange'
+# 'kiwi'
