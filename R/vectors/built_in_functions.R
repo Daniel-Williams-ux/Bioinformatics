318ce5177a7c15cbcb02382c11accb16f5bc92b8
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-07 13:28:09 +0100

    Create built_in_functions.R

---
+++ R/vectors/built_in_functions.R
@@ -0,0 +1,36 @@
+#Universal (built-in) functions that can be applied to vectors
+# Create a vector "v":
+​
+v = c(25, 9, 16, 4)
+v
+# 1. 
+mean(v)
+# Return the mean value of the vector
+​
+# 2.
+sqrt(v)
+# Return the square root of each value in the vector
+​
+# 3.
+max(v)
+# Return the maximum value in the vector
+​
+# 4.
+min(v)
+# Return the smallest value in the vector
+​
+# 5.
+sum(v)
+# Return the sum of the values in the vector
+​
+# 6.
+sort(v)
+# Sort the values in a vector in ascending order
+​
+# 7.
+sort(v, decreasing=TRUE)
+# Sort the values in a vector in descending order
+​
+# 8.
+table(c(20,30,50,20,40,10,40,20))
+# Count the frequency of occurrence for each element in a vector:
