283d49847d43a851ca39ea95fb071ab3286d7044
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-09 20:05:51 +0100

    Create csv_file.R

---
+++ importing_data/csv_file.R
@@ -0,0 +1,3 @@
+# Comma separated values (csv) is a common way that data can be formatted. To import a csv file, read.csv() is used. The name of the file is passed in and the dataset will be imported as a data frame.
+df <- read.csv("data/heart_disease.csv")
+# Note: Comma Separated Values (CSV) is one of the most common file formats for storing data, and CSV files are supported by nearly all data upload interfaces.
