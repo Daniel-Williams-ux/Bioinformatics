9de346f55abe30511ba3646d27b9fd400afaa481
Daniel Williams <williamsoligodaniel@gmail.com>
2024-09-12 10:40:52 +0100

    Create Lesson_1_Z-score.py

---
+++ introduction to statistics II/Lesson_1_Z-score.py
@@ -0,0 +1,4 @@
+                                                                                                             Normal Distribution
+We say that a dataset has a normal distribution if its values fall into a smooth (continuous), bell-shaped curve with a symmetric pattern--each side looks the same when cut down the middle. The normal distribution is extremely important to statistics. 
+One reason that it's so useful is that it enables us to determine the probability of something occuring due to chance. For example, it enables us to determine the probability that a given data point will fall a given distance from the distribution's mean.
+Refer to  Normal Distribution.png
